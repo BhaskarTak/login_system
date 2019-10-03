24bfc1ed758dfa5a4884ca7189566de94165b7ab
bhaskartak89 <bhaskartak89@gmail.com>
2019-10-03 21:07:14 +0530

    Added file

---
+++ login_system.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# ### Login System
+# 
+# Creating a basic login system which will ask for username and password and will match it up with the one's existing in the database.
+# 
+# STEP-1
+
+# In[4]:
+
+
+import getpass
+# Setting a default username and password
+username="admin"
+password="thenewdogwillbite"
+
+# Taking the input of username
+u=input("Username: ")
+
+if u==username:
+    
+# Taking password as input
+    p=getpass.getpass('Password: ')
+    if p==password:
+        print("Logged in sucessfully!")
+    else:
+        print("Incorrect password")
+    
+else:
+    print('Invalid Username')
+
